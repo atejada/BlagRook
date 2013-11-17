433c9be12bbae6abd09af982b11ee15f888a2f8e
unknown <I830502@PALN00533749A.sap.corp>
2013-11-17 10:24:37 -0800

    message

--- demo.R
+++ demo.R
@@ -12,11 +12,9 @@ newapp<-function(env){
   dev.off()
   
   to.read = file("R_Plot.bmp", "rb")
-  readBin(to.read, raw(),n=1L)
   x<-readBin(to.read, raw(),n=231488)
   test<-paste(x, collapse = "")
 
-  print(test)
   res$write(test)
  
   res$finish()
