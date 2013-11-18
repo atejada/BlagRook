66a34278f73473bbcb1ac91e07069cf50e80efd3
unknown <I830502@PALN00533749A.sap.corp>
2013-11-18 14:26:42 -0800

    message

--- demo.R
+++ demo.R
@@ -1,23 +1,23 @@
 library(Rook)
  
 newapp<-function(env){
-  req<-Rook::Request$new(env)
-  res<-Rook::Response$new()
+ req<-Rook::Request$new(env)
+ res<-Rook::Response$new()
  
-  carrid_param<-c(req$params()$carrid)
-  seats_param<-c(req$params()$seats)
+ carrid_param<-c(req$params()$carrid)
+ seats_param<-c(req$params()$seats)
 
-   bmp("R_Plot.bmp",type=c("cairo"))
-   barplot(seats_param,names.arg=carrid_param)
+  bmp("R_Plot.bmp",type=c("cairo"))
+  barplot(seats_param,names.arg=carrid_param)
 #  graph<-data.frame(c(1,2,3),c(4,5,6))
 #  plot(graph)
-   dev.off()
+  dev.off()
   
-   to.read = file("R_Plot.bmp", "rb")
-   x<-readBin(to.read, raw(),n=231488)
-   hex<-paste(x, collapse = "")
+  to.read = file("R_Plot.bmp", "rb")
+  x<-readBin(to.read, raw(),n=231488)
+  hex<-paste(x, collapse = "")
 
-   res$write(hex)
+  res$write(hex)
  
   res$finish()
 }
